677b45690c88f2b0561b4adfe1a0403c00685d75
gdev2018 <gdev2018@gmail.com>
2019-03-27 08:01:31 +0300

    raplace Copyright

--- fnd_app_base/src/main/java/com/github/gdev2018/master/support/widget/DefaultItemAnimator.java
+++ fnd_app_base/src/main/java/com/github/gdev2018/master/support/widget/DefaultItemAnimator.java
@@ -1,9 +1,17 @@
 /*
- * This is the source code of Powerlabor (TM) for Android
- * It is licensed under GNU GPL v. 2 or later.
- * You should have received a copy of the license in this archive (see LICENSE).
+ * Copyright (C) 2014 The Android Open Source Project
  *
- * Copyright Livesey Inc. (c) 2007-2018.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.gdev2018.master.support.widget;
 
--- fnd_app_base/src/main/java/com/github/gdev2018/master/support/widget/LinearSmoothScrollerMiddle.java
+++ fnd_app_base/src/main/java/com/github/gdev2018/master/support/widget/LinearSmoothScrollerMiddle.java
@@ -1,9 +1,9 @@
 /*
- * This is the source code of Powerlabor (TM) for Android
+ * This is the source code of Telegram for Android v. 5.x.x.
  * It is licensed under GNU GPL v. 2 or later.
  * You should have received a copy of the license in this archive (see LICENSE).
  *
- * Copyright Livesey Inc. (c) 2007-2018.
+ * Copyright Nikolai Kudashov, 2013-2018.
  */
 
 package com.github.gdev2018.master.support.widget;

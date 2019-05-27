0585e1dc70285504fd37e97062e2d34e7136df1b
gdev2018 <gdev2018@gmail.com>
2019-05-27 07:12:27 +0300

    run TestActivity

--- app/src/main/java/gdev2018/github/com/fnd_app_base/TestActivity.java
+++ app/src/main/java/gdev2018/github/com/fnd_app_base/TestActivity.java
@@ -3,7 +3,15 @@ package gdev2018.github.com.fnd_app_base;
 import android.os.Bundle;
 import android.app.Activity;
 
+import android.widget.TextView;
+
+
+import static com.github.gdev2018.master.AndroidUtilities.secondsToString;
+
+
 public class TestActivity extends Activity {
+
+    private String seconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +19,16 @@ public class TestActivity extends Activity {
         setContentView(R.layout.activity_test);
 
 
+        seconds = secondsToString(123);
+
+
+        ((TextView)findViewById(R.id.seconds_textView)).setText(seconds);
+
+
+
+
     }
+
+    public static native int native_isTestBackend(int currentAccount);
 
 }

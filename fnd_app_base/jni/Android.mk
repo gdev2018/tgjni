0e34826981a2d490d9ea02b722ce982db83f1915
gdev2018 <gdev2018@gmail.com>
2019-08-22 09:01:29 +0300

    md5 LOCAL_C_INCLUDES

--- fnd_app_base/jni/Android.mk
+++ fnd_app_base/jni/Android.mk
@@ -135,12 +135,10 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE  := arm
-LOCAL_MODULE := sqlite
 LOCAL_CFLAGS 	:= -w -std=c11 -Os -DNULL=0 -DSOCKLEN_T=socklen_t -DLOCALE_NOT_USED -D_LARGEFILE_SOURCE=1
 LOCAL_CFLAGS 	+= -DANDROID_NDK -DDISABLE_IMPORTGL -fno-strict-aliasing -fprefetch-loop-arrays -DAVOID_TABLES -DANDROID_TILE_BASED_DECODE -DANDROID_ARMV6_IDCT -DHAVE_STRCHRNUL=0
-
-LOCAL_C_INCLUDES    := \
-./sqlite/md5.h
+LOCAL_C_INCLUDES += ./sqlite
+LOCAL_MODULE := sqlite
 
 LOCAL_SRC_FILES     := \
 ./sqlite/sqlite3.c \
